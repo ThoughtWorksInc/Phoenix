2e0bccc503b2c73f5372cdd4910d45294bdadae9
Sam Newman <sam.newman@gmail.com>
2012-06-19 12:05:11 +0100

    Added basic details page

--- phoenix/web/phoweb.py
+++ phoenix/web/phoweb.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from werkzeug.exceptions import abort
 from phoenix import fabfile
 from phoenix.plogging import logger
 
@@ -12,6 +13,20 @@ property_file="../../build_credentials/phoenix.ini"
 def hello():
     definitions = fabfile.get_list_of_environment_definitions(config_dir=config_location, property_file=property_file)
     return render_template('index.html', definitions=definitions)
+
+@app.route("/details/<defname>")
+def definition_details(defname):
+    definitions = fabfile.get_list_of_environment_definitions(config_dir=config_location, property_file=property_file)
+
+    for definition in definitions:
+        if definition.name == defname:
+            return render_template("def_details.html", definition=definition)
+
+    return page_not_found("No such template %s" % defname)
+
+@app.errorhandler(404)
+def page_not_found(user_visible_error):
+    return render_template('404.html', user_visible_error=user_visible_error), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
